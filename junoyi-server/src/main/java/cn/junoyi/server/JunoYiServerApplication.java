a32530b804382df6a17bf85d22c26cd05857b339
EatFan <eatfan0921@163.com>
2025-11-23 21:09:23 +0800

    feat(server): 配置Spring Boot应用扫描包路径
    - 添加了scanBasePackages配置以指定组件扫描范围
    - 更新了类注释以说明服务启动功能
    - 明确指定了基础包路径为cn.junoyi以优化启动性能

--- junoyi-server/src/main/java/cn/junoyi/server/JunoYiServerApplication.java
+++ junoyi-server/src/main/java/cn/junoyi/server/JunoYiServerApplication.java
@@ -4,7 +4,12 @@ package cn.junoyi.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * 启动服务端
+ *
+ * @author Fan
+ */
+@SpringBootApplication(scanBasePackages= {"cn.junoyi"})
 public class JunoYiServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(JunoYiServerApplication.class);
