d3370f29f2f6b1de4e8d3f0d32b9987285c107ab
EatFan <eatfan0921@163.com>
2025-12-15 21:13:18 +0800

    feat(event): 添加Spring应用上下文事件封装与适配器
    - 新增SpringApplicationContextEvent类用于封装应用上下文事件
    - 修改SpringContextClosedEvent继承SpringApplicationContextEvent以复用逻辑
    - 新增SpringContextRefreshedEvent类表示上下文刷新事件
    - 添加SpringContextRefreshedEventAdapter适配器转换Spring原生事件
    - 优化事件适配器接口实现与事件转换逻辑

--- junoyi-framework/junoyi-framework-event/src/main/java/com/junoyi/framework/event/domain/spring/SpringContextClosedEvent.java
+++ junoyi-framework/junoyi-framework-event/src/main/java/com/junoyi/framework/event/domain/spring/SpringContextClosedEvent.java
@@ -1,6 +1,5 @@
 package com.junoyi.framework.event.domain.spring;
 
-import com.junoyi.framework.event.domain.BaseEvent;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -9,23 +8,13 @@ import org.springframework.context.ApplicationContext;
  *
  * @author Fan
  */
-public class SpringContextClosedEvent extends BaseEvent {
-
-    private final Object source;
+public class SpringContextClosedEvent extends SpringApplicationContextEvent {
 
     /**
      * 构造方法，创建一个新的Spring上下文关闭事件
      * @param source 触发事件的应用上下文对象，不能为空
      */
     public SpringContextClosedEvent(ApplicationContext source){
-        this.source = source;
-    }
-
-    /**
-     * 获取应用上下文对象
-     * @return 返回触发此事件的ApplicationContext实例
-     */
-    public final ApplicationContext getApplicationContext(){
-        return (ApplicationContext) source;
+        super(source);
     }
 }
