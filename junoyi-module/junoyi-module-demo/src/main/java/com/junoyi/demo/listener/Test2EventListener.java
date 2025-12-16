7cd22d61ca2aa59546b7d790be782746d55035e4
EatFan <eatfan0921@163.com>
2025-12-16 18:44:07 +0800

    fix(event): 移除SpringApplicationStartingEvent及相关适配器
    - 删除SpringApplicationStartingEvent类及其相关代码
    - 移除SpringApplicationStartingEventAdapter适配器实现
    - 清理Test2EventListener中的无用事件监听逻辑
    - 更新开发日志记录此次变更内容

--- junoyi-module/junoyi-module-demo/src/main/java/com/junoyi/demo/listener/Test2EventListener.java
+++ junoyi-module/junoyi-module-demo/src/main/java/com/junoyi/demo/listener/Test2EventListener.java
@@ -1,17 +1,8 @@
 package com.junoyi.demo.listener;
 
-import com.junoyi.framework.event.annotation.EventHandler;
 import com.junoyi.framework.event.core.Listener;
-import com.junoyi.framework.event.domain.spring.SpringApplicationStartingEvent;
 
 public class Test2EventListener implements Listener {
-
-    @EventHandler
-    public void onTestEven32t(SpringApplicationStartingEvent event){
-        System.out.println("===============");
-        System.out.println(" 测试  SpringApplicationStartingEvent 事件监听");
-        System.out.println("===============");
-    }
 
 
 }
