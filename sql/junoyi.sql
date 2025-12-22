2b740cbaa5791ef5f35171fbaef5c8c7307ebc86
EatFan <eatfan0921@163.com>
2025-12-22 09:51:42 +0800

    feat(sql): 更新SQL数据库

--- sql/junoyi.sql
+++ sql/junoyi.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80404 (8.4.4)
  File Encoding         : 65001
 
- Date: 05/12/2025 08:32:03
+ Date: 22/12/2025 09:50:57
 */
 
 SET NAMES utf8mb4;
@@ -35,7 +35,7 @@ CREATE TABLE `sys_role` (
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `remark` text COMMENT '备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系统角色表';
 
 -- ----------------------------
 -- Records of sys_role
@@ -51,19 +51,16 @@ COMMIT;
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
   `user_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `openid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '小程序绑定id',
   `user_name` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户账号',
   `nick_name` varchar(100) DEFAULT NULL COMMENT '用户昵称',
-  `user_type` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户类型（00系统用户， 01 系统注册用户，02小程序用户',
-  `emaili` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
   `phonenumber` varchar(11) DEFAULT NULL COMMENT '手机号',
   `sex` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '0' COMMENT '性别(0 未设定， 1男，2女）',
   `password` varchar(255) DEFAULT NULL COMMENT '密码（加密后）',
-  `salt` varchar(30) DEFAULT NULL COMMENT '加密盐',
-  `stauts` int DEFAULT '1' COMMENT '状态（1正常，0停用）',
+  `salt` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '加密盐',
+  `status` int DEFAULT '1' COMMENT '状态（1正常，0停用）',
   `del_flag` tinyint DEFAULT '0' COMMENT '软删除标识符号（0未删除，1删除）',
-  `login_ip` varchar(128) DEFAULT NULL COMMENT '登录ip记录',
-  `login_time` datetime DEFAULT NULL COMMENT '登录时间',
   `pwd_update_time` datetime DEFAULT NULL COMMENT '密码修改时间',
   `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
@@ -71,13 +68,58 @@ CREATE TABLE `sys_user` (
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `remark` text COMMENT '备注',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_user` (`user_id`, `openid`, `user_name`, `nick_name`, `user_type`, `emaili`, `phonenumber`, `sex`, `password`, `salt`, `stauts`, `del_flag`, `login_ip`, `login_time`, `pwd_update_time`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1, NULL, 'super_admin', '超级管理员', '00', 'exmple@junoyi.com', '18899887878', '1', '29c67a30398638269fe600f73a054934', '111111', 1, 0, '127.0.0.1', '2025-12-05 08:12:41', NULL, 'system', '2025-12-05 08:13:00', 'system', '2025-12-05 08:13:17', '超级管理员');
+INSERT INTO `sys_user` (`user_id`, `user_name`, `nick_name`, `avatar`, `email`, `phonenumber`, `sex`, `password`, `salt`, `status`, `del_flag`, `pwd_update_time`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1, 'admin', '超级管理员', NULL, 'exmple@junoyi.com', '18899887878', '1', 'm/ctuGNjUwrpOxdqrd2fQsfVN1Mnbu6EKwJWXN+P3W4=', '3dvSoCjGtCXZnSB+6ENWtQ==', 1, 0, NULL, 'system', '2025-12-05 08:13:00', 'system', '2025-12-05 08:13:17', '超级管理员');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for sys_user_other_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_other_auth`;
+CREATE TABLE `sys_user_other_auth` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `user_id` bigint DEFAULT NULL COMMENT '用户ID',
+  `auth_type` varchar(20) DEFAULT NULL COMMENT '登录类型',
+  `auth_key` varchar(255) DEFAULT NULL COMMENT '平台唯一标识符',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户第三登录绑定表';
+
+-- ----------------------------
+-- Records of sys_user_other_auth
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for sys_user_platform
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_platform`;
+CREATE TABLE `sys_user_platform` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `platform_type` int DEFAULT NULL COMMENT '登录终端平台类型（0后台Web，1前台Web，2小程序，3APP，4桌面端）',
+  `platform_uid` varchar(255) DEFAULT NULL COMMENT '平台唯一标识符',
+  `login_ip` varchar(128) DEFAULT NULL COMMENT '登录IP',
+  `login_time` datetime DEFAULT NULL COMMENT '登录时间',
+  `token` varchar(255) DEFAULT NULL COMMENT '登录使用到的token',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户登录终端信息表';
+
+-- ----------------------------
+-- Records of sys_user_platform
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_user_platform` (`id`, `user_id`, `platform_type`, `platform_uid`, `login_ip`, `login_time`, `token`) VALUES (1, 1, 0, 'admin_web', '127.0.0.1', '2025-12-18 21:19:35', 'fdafdasfdasfdafdsafdsa');
 COMMIT;
 
 -- ----------------------------
@@ -89,7 +131,7 @@ CREATE TABLE `sys_user_role` (
   `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `role_id` bigint DEFAULT NULL COMMENT '角色id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户角色数据关联表';
 
 -- ----------------------------
 -- Records of sys_user_role
