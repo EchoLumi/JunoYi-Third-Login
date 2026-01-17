a17a26e6f273bf39a0b84876ead20b107eec8f40
EatFan <eatfan0921@163.com>
2026-01-17 17:43:13 +0800

    feat(mapper): 为系统模块映射器添加数据范围忽略注解
    - 在SysDeptGroupMapper接口上添加@IgnoreDataScope注解
    - 在SysPermGroupMapper接口上添加@IgnoreDataScope注解
    - 在SysRoleGroupMapper接口上添加@IgnoreDataScope注解
    - 在SysUserDeptMapper接口上添加@IgnoreDataScope注解
    - 在SysUserGroupMapper接口上添加@IgnoreDataScope注解
    - 在SysUserPermMapper接口上添加@IgnoreDataScope注解
    - 在SysUserRoleMapper接口上添加@IgnoreDataScope注解
    - 导入IgnoreDataScope注解到所有相关映射器类中

--- junoyi-module-api/junoyi-module-system-api/src/main/java/com/junoyi/system/mapper/SysUserDeptMapper.java
+++ junoyi-module-api/junoyi-module-system-api/src/main/java/com/junoyi/system/mapper/SysUserDeptMapper.java
@@ -1,6 +1,7 @@
 package com.junoyi.system.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.junoyi.framework.datasource.datascope.annotation.IgnoreDataScope;
 import com.junoyi.system.domain.po.SysUserDept;
 import org.apache.ibatis.annotations.Mapper;
 
